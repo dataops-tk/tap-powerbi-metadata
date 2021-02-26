dda0593546474cbdf29ffc4a594e2407e417e3ca
John Timeus <john.timeus@slalom.com>
2021-02-26 10:41:52 -0800

    refine auth and ActivityEventsStream

--- tap_powerbi_metadata/streams.py
+++ tap_powerbi_metadata/streams.py
@@ -1,8 +1,9 @@
 """Stream class for tap-powerbi-metadata."""
 
 from copy import deepcopy
+from datetime import timedelta
 from pathlib import Path
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Iterable, Optional
 import requests
 from singer_sdk.helpers.util import utc_now
 
@@ -30,7 +31,7 @@ class OAuthActiveDirectoryAuthenticator(OAuthAuthenticator):
             'scope': 'https://api.powerbi.com',
             'resource': 'https://analysis.windows.net/powerbi/api',
             'client_id': self.config["client_id"],
-            'username': self.config.get("username", self.config["client_id"]),
+            'username': self.config["username"],
             'password': self.config["password"],
         }
 
@@ -45,7 +46,11 @@ class TapPowerBIMetadataStream(RESTStream):
         params = {}
         starting_datetime = self.get_starting_datetime(partition)
         if starting_datetime:
-            params.update({"startDateTime": starting_datetime})
+            params.update({"startDateTime": starting_datetime.strftime("'%Y-%m-%dT%H:%M:%SZ'")})
+            #TODO need to loop over multiple days until done (https://github.com/dataops-tk/tap-powerbi-metadata/issues/5)
+            ending_datetime = starting_datetime + timedelta(hours=9)
+            params.update({"endDateTime": ending_datetime.strftime("'%Y-%m-%dT%H:%M:%SZ'")})
+        self.logger.info(params)
         return params
 
     @property
@@ -56,17 +61,28 @@ class TapPowerBIMetadataStream(RESTStream):
             oauth_scopes="https://analysis.windows.net/powerbi/api",
         )
 
-    def get_next_page_token(self, response: dict) -> Optional[Any]:
+    def get_next_page_token(self, response) -> Optional[Any]:
         """Return token for identifying next page or None if not applicable."""
-        return response.get("continuationToken")
+        #TODO Continuation Token not properly parsed (https://github.com/dataops-tk/tap-powerbi-metadata/issues/4)
+        #resp_json = response.json()
+        #self.logger.info(resp_json.get("continuationUri"))
+        #return resp_json.get("continuationToken")
+        return None
 
     def insert_next_page_token(self, next_page, params) -> Any:
         """Inject next page token into http request params."""
         if (not next_page) or next_page == 1:
             return params
-        params["continuationToken"] = next_page
+        params.pop("startDateTime")
+        params.pop("endDateTime")
+        params["continuationToken"] = "'" + next_page + "'"
         return params
 
+    def parse_response(self, response) -> Iterable[dict]:
+        """Parse the response and return an iterator of result rows."""
+        resp_json = response.json()
+        for row in resp_json.get("activityEventEntities"):
+            yield row
 
 class ActivityEventsStream(TapPowerBIMetadataStream):
     name = "ActivityEvents"
@@ -75,33 +91,33 @@ class ActivityEventsStream(TapPowerBIMetadataStream):
     replication_key = "CreationTime"
     schema = PropertiesList(
         StringType("Id"),
-        StringType("RecordType"),
+        IntegerType("RecordType", optional=True),
         DateTimeType("CreationTime"),
-        StringType("Operation"),
-        StringType("OrganizationId"),
-        StringType("UserType"),
-        StringType("UserKey"),
-        StringType("Workload"),
-        StringType("UserId"),
-        StringType("ClientIP"),
-        StringType("UserAgent"),
-        StringType("Activity"),
-        StringType("ItemName"),
-        StringType("CapacityId"),
-        StringType("CapacityName"),
-        StringType("WorkspaceId"),
-        StringType("WorkSpaceName"),
-        StringType("DatasetId"),
-        StringType("DatasetName"),
-        StringType("ReportId"),
-        StringType("ReportName"),
-        StringType("ObjectId"),
-        BooleanType("IsSuccess"),
-        StringType("ReportType"),
-        StringType("RequestId"),
-        StringType("ActivityId"),
-        StringType("AppName"),
-        StringType("AppReportId"),
-        StringType("DistributionMethod"),
-        StringType("ConsumptionMethod"),
+        StringType("Operation", optional=True),
+        StringType("OrganizationId", optional=True),
+        IntegerType("UserType", optional=True),
+        StringType("UserKey", optional=True),
+        StringType("Workload", optional=True),
+        StringType("UserId", optional=True),
+        StringType("ClientIP", optional=True),
+        StringType("UserAgent", optional=True),
+        StringType("Activity", optional=True),
+        StringType("ItemName", optional=True),
+        StringType("CapacityId", optional=True),
+        StringType("CapacityName", optional=True),
+        StringType("WorkspaceId", optional=True),
+        StringType("WorkSpaceName", optional=True),
+        StringType("DatasetId", optional=True),
+        StringType("DatasetName", optional=True),
+        StringType("ReportId", optional=True),
+        StringType("ReportName", optional=True),
+        StringType("ObjectId", optional=True),
+        BooleanType("IsSuccess", optional=True),
+        StringType("ReportType", optional=True),
+        StringType("RequestId", optional=True),
+        StringType("ActivityId", optional=True),
+        StringType("AppName", optional=True),
+        StringType("AppReportId", optional=True),
+        StringType("DistributionMethod", optional=True),
+        StringType("ConsumptionMethod", optional=True),
     ).to_dict()
